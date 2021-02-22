eb33d72a7d21935c8ad27bd6bd7f249524ad0554
mehran-ahmadzadeh <ahmadzadeh_dev@protonmail.com>
2021-02-22 17:51:28 +0300

    add header style and fix layout

--- static/base/main.js
+++ static/base/main.js
@@ -3,17 +3,11 @@
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId);
     const nav = document.getElementById(navId);
-    const main = document.getElementById("main_page");
-    const header = document.getElementById("page_header");
-    const footer = document.getElementById("page_footer");
 
     if (toggle && nav) {
         toggle.addEventListener("click", () => {
             nav.classList.toggle("open_menu");
             toggle.classList.toggle("bx-x");
-            main.classList.toggle("hide");
-            header.classList.toggle("hide");
-            footer.classList.toggle("hide");
         });
     }
 };

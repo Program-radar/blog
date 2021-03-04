07a037aa9244a81f12a55c7b89c093c4df58bb46
mehran <program_who@protonmail.com>
2021-03-04 18:15:35 +0330

    changed links from windows into unix

--- blogging/views.py
+++ blogging/views.py
@@ -1,12 +1,14 @@
 from django.views.generic import ListView, DetailView
 from .models import Article
 
+
 class Index(ListView):
     model = Article
     context_object_name = 'articles'
-    template_name = 'blogging\\index.html'
+    template_name = 'blogging/index.html'
+
 
 class BlogDetailView(DetailView):
     model = Article
     context_object_name = 'articles'
-    template_name = 'blogging\\article_detail.html'
+    template_name = 'blogging/article_detail.html'

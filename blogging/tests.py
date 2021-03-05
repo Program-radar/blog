1292a8f478103b2de829fc74d0ff2bbd6b544d50
mehran <program_who@protonmail.com>
2021-03-05 10:53:13 +0330

    fix tests

--- blogging/tests.py
+++ blogging/tests.py
@@ -5,8 +5,9 @@ from .models import Article
 
 
 class BlogTests(TestCase):
+
     def setUp(self):
-        self.user = get_user_model.objects.create_user(
+        self.user = get_user_model().objects.create_user(
             username='testuser',
             email='test@email.com',
             password='secret'
@@ -14,22 +15,21 @@ class BlogTests(TestCase):
 
         self.article = Article.objects.create(
             title='this is title',
-            body='this is body.this is body.this.'
+            body='this is body.this is body.this.',
             author=self.user,
         )
 
     def test_string_representation(self):
-        post = Post(title='sample title')
-        self.assertEqual(str(post), post.title)
+        article = Article(title='sample title')
+        self.assertEqual(str(article), article.title)
 
     def test_post_content(self):
-        self.assertEqual(f'{self.post.author}', 'testuser')
-        self.assertEqual(f'{self.post.title}', 'this is title')
-        self.assertEqual(f'{self.post.body}',
+        self.assertEqual(f'{self.article.author}', 'testuser')
+        self.assertEqual(f'{self.article.title}', 'this is title')
+        self.assertEqual(f'{self.article.body}',
                          'this is body.this is body.this.')
 
     def test_home_page(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'blogging/index.html')
-        self.assertContains(response, 'this is body.this is body.this.')

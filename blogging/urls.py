1ae593ca4f01461df6f18671839d2129c52e51af
mehran <program_who@protonmail.com>
2021-03-06 16:58:56 +0330

    add new post form

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,8 +1,9 @@
 """Blogging App Urls Patterns"""
 from django.urls import path
-from .views import Index, BlogDetailView
+from .views import Index, BlogDetailView, BlogCreateView
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('posts/<int:pk>', BlogDetailView.as_view(), name='article_detail'),
+    path('posts/new', BlogCreateView.as_view(), name='new_post')
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Article
 
 
@@ -12,3 +13,10 @@ class BlogDetailView(DetailView):
     model = Article
     context_object_name = 'articles'
     template_name = 'blogging/article_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Article
+    context_object_name = 'articles'
+    template_name = 'blogging/new_post.html'
+    fields = ['title', 'author', 'body']

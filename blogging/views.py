34c0ec9a8bb97879a2f2eaa32c983d4e7a3e2286
mehran <program_who@protonmail.com>
2021-03-10 14:49:24 +0330

    add context_name variable

--- blogging/views.py
+++ blogging/views.py
@@ -2,21 +2,23 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from .models import Article
 
+context_name = 'articles'
+
 
 class Index(ListView):
     model = Article
-    context_object_name = 'articles'
+    context_object_name = context_name
     template_name = 'blogging/index.html'
 
 
 class BlogDetailView(DetailView):
     model = Article
-    context_object_name = 'articles'
+    context_object_name = context_name
     template_name = 'blogging/article_detail.html'
 
 
 class BlogCreateView(CreateView):
     model = Article
-    context_object_name = 'articles'
+    context_object_name = context_name
     template_name = 'blogging/new_post.html'
     fields = ['title', 'author', 'body']
